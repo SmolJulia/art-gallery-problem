8163e3e569d1ef56feba6d08ab3a6f610eebdcd9
SmolJulia <sweetkitten2000@wp.pl>
2024-12-16 21:53:43 +0100

    Update test_wprowadzanie.py

--- tests/test_wprowadzanie.py
+++ tests/test_wprowadzanie.py
@@ -1,6 +1,6 @@
 import os
-from agp_project import liczba_straznikow #agp_project.main
-from pliki_zrodlowe import file_path #agp_project.pliki_zrodlowe.wprowadzanie_pliku
+from agp_project.main import liczba_straznikow #agp_project.main
+from agp_project.pliki_zrodlowe.wprowadzanie_pliku import file_path #agp_project.pliki_zrodlowe.wprowadzanie_pliku
 def test_kwadrat(self):
   l = liczba_straznikow
   file_name = os.path.basename(file_path)
