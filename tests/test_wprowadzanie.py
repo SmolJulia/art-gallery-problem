8ffdcf0eb3d8cc096b58724c23325e506b1b282e
SmolJulia <sweetkitten2000@wp.pl>
2024-12-16 22:17:58 +0100

    Update test_wprowadzanie.py

--- tests/test_wprowadzanie.py
+++ tests/test_wprowadzanie.py
@@ -2,10 +2,10 @@ import os
 from agp_project.main import * #agp_project.main #agp_project.pliki_zrodlowe.wprowadzanie_pliku
 from agp_project.pliki_zrodlowe.wprowadzanie_pliku import *
 
-def test_kwadrat():
-  l = liczba_straznikow
-  file_name = os.path.basename(file_path)
-  
+l = liczba_straznikow
+file_name = os.path.basename(file_path)
+
+def test_kwadrat():  
   if file_name == "kwadrat.csv":
     assert l == 1
 
