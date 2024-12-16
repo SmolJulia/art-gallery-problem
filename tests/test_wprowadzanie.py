02419ef5b849bfcf963a181e70d6fd1c7b0bbfa0
SmolJulia <sweetkitten2000@wp.pl>
2024-12-16 21:34:12 +0100

    Update and rename test_wprowadzanie to test_wprowadzanie.py

---
+++ tests/test_wprowadzanie.py
@@ -0,0 +1,10 @@
+import os
+from agp_project import liczba_straznikow #agp_project.main
+from pliki_zrodlowe import file_path #agp_project.pliki_zrodlowe.wprowadzanie_pliku
+def test_kwadrat(self):
+  l = liczba_straznikow
+  file_name = os.path.basename(file_path)
+  
+  if file_name == kwadrat.csv:
+    assert l == 1
+
