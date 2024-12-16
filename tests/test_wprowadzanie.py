645cf5d72cac67036946aee82ee70dfdd484273d
SmolJulia <sweetkitten2000@wp.pl>
2024-12-16 22:12:11 +0100

    Update test_wprowadzanie.py

--- tests/test_wprowadzanie.py
+++ tests/test_wprowadzanie.py
@@ -1,5 +1,6 @@
 import os
-from agp_project import * #agp_project.main #agp_project.pliki_zrodlowe.wprowadzanie_pliku
+from agp_project.main import * #agp_project.main #agp_project.pliki_zrodlowe.wprowadzanie_pliku
+from agp_project.pliki_zrodlowe.wprowadzanie_pliku import *
 
 def test_kwadrat():
   l = liczba_straznikow
