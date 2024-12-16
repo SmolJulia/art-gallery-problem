9116eedccf1f637e9a372616aee3573b324cafa9
SmolJulia <sweetkitten2000@wp.pl>
2024-12-17 00:28:48 +0100

    Update test_wprowadzanie.py

--- tests/test_wprowadzanie.py
+++ tests/test_wprowadzanie.py
@@ -1,9 +1,9 @@
 import os
-from agp_project.main import * #agp_project.main #agp_project.pliki_zrodlowe.wprowadzanie_pliku
+from agp_project.pliki_zrodlowe.triangulacja import oblicz_liczbe_straznikow #agp_project.main #agp_project.pliki_zrodlowe.wprowadzanie_pliku
 from agp_project.pliki_zrodlowe.wprowadzanie_pliku import *
 
+l = oblicz_liczbe_straznikow()
 file_name = os.path.basename(file_path)
-l = liczba_straznikow
 
 def test_kwadrat():  
   if file_name == "kwadrat.csv":
