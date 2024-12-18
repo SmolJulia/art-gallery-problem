5a96e0a8bb08fcd0d62c4a1f8e6352cd6493cd3e
SmolJulia <sweetkitten2000@wp.pl>
2024-12-18 18:53:09 +0100

    Update test_wprowadzanie.py

--- test_art gallery problem/test_wprowadzanie.py
+++ test_art gallery problem/test_wprowadzanie.py
@@ -3,26 +3,10 @@ from agp_project.pliki_zrodlowe.triangulacja import oblicz_liczbe_straznikow
 from agp_project.pliki_zrodlowe.wprowadzanie_pliku import *
 from agp_project.main import *
 
-os.chdir(os.path.dirname(os.path.abspath(__file__)))
-
-
-l = oblicz_liczbe_straznikow()
-file_name = os.path.basename(filepath)
+l = oblicz_liczbe_straznikow('pliki_wielokat/kwadrat.csv')
+file_name = os.path.basename(file_path)
 
 class TestWprowadzanie:  
   def Test_plik_kwadrat(self):
-        fake_file_path = os.path.abspath('agp_project/pliki_wielokat/kwadrat.csv')
-        mock_askopenfilename.return_value = fake_file_path
-
-        # Simulate the file selection
-        file_path = plik_z_okna_dialogowego()
-        
-        # Extract the file name and check if it's 'kwadrat.csv'
-        file_name = os.path.basename(file_path)
-
-        if file_name == "kwadrat.csv":
-            # Run the function with the mocked file path (which doesn't exist on disk)
-            l = oblicz_liczbe_straznikow(file_path)
-            
-            # Check that the result is what we expect for 'kwadrat.csv'
-            assert l == 1
+    if file_name == "kwadrat.csv":
+      assert l == 1
