e59e481cc733e595373b32195cd47ccae08dbada
SmolJulia <sweetkitten2000@wp.pl>
2024-12-18 17:16:40 +0100

    Update test_wprowadzanie.py

--- test_art gallery problem/test_wprowadzanie.py
+++ test_art gallery problem/test_wprowadzanie.py
@@ -1,11 +1,11 @@
 import os
 from agp_project.pliki_zrodlowe.triangulacja import oblicz_liczbe_straznikow
-from agp_project.pliki_zrodlowe.wprowadzanie_pliku import plik_z_okna_dialogowego
+from agp_project.pliki_zrodlowe.wprowadzanie_pliku import *
 from agp_project.main import *
-
+os.chdir('agp_project/pliki_wielokat')
 
 l = oblicz_liczbe_straznikow()
-file_name = os.path.basename(plik_z_okna_dialogowego())
+file_name = os.path.basename(file_path)
 
 class TestWprowadzanie:  
   def Test_plik_kwadrat(self):
