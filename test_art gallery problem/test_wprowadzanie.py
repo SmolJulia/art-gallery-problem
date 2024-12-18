691a8a986fbf35b876e0235295d73d1d13ba5b97
SmolJulia <sweetkitten2000@wp.pl>
2024-12-18 17:22:11 +0100

    Update test_wprowadzanie.py

--- test_art gallery problem/test_wprowadzanie.py
+++ test_art gallery problem/test_wprowadzanie.py
@@ -4,10 +4,9 @@ from agp_project.pliki_zrodlowe.wprowadzanie_pliku import *
 from agp_project.main import *
 os.chdir('agp_project/pliki_wielokat')
 
-l = oblicz_liczbe_straznikow()
 file_name = os.path.basename(filepath)
 
 class TestWprowadzanie:  
   def Test_plik_kwadrat(self):
     if file_name == "kwadrat.csv":
-      assert l == 1
+      assert liczba_straznikow == 1
