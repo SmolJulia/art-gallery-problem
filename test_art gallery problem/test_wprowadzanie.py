6cb668e12097d8a257da90f52b7e6e850a9aed6e
SmolJulia <sweetkitten2000@wp.pl>
2025-01-14 21:39:27 +0100

    Update test_wprowadzanie.py

--- test_art gallery problem/test_wprowadzanie.py
+++ test_art gallery problem/test_wprowadzanie.py
@@ -1,9 +1,9 @@
 import os
-from agp_project.pliki_zrodlowe.triangulacja import oblicz_liczbe_straznikow
-from agp_project.pliki_zrodlowe.wprowadzanie_pliku import *
-from agp_project.main import *
+from test_agp_project.test_pliki_zrodlowe.triangulacja import oblicz_liczbe_straznikow
+from test_agp_project.test_pliki_zrodlowe.wprowadzanie_pliku import *
+from test_agp_project.main import *
 
-l = oblicz_liczbe_straznikow('pliki_wielokat/kwadrat.csv')
+l = oblicz_liczbe_straznikow('test_pliki_wielokat/kwadrat.csv')
 file_name = os.path.basename(file_path)
 
 class TestWprowadzanie:  
